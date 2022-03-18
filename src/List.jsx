bbd8b4c86ee85c74b183ab802b9466b2b063de77
JoaquinJPB <jjpb2000@gmail.com>
2022-03-18 13:49:21 +0000

    Birthday complete ( useState() method and how it works)

--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,18 @@
 import React, { Fragment } from 'react';
 
-const List = () => {
+const List = ({people}) => {
     return (
         <Fragment>
-            <h2>list component</h2>
+            {people.map((person) => {
+                const {id, name, age, image} = person;
+                return <article key={id} className="person">
+                    <img src={image} alt={name} />
+                    <div>
+                        <h4>{name}</h4>
+                        <p>{age} years</p>
+                    </div>
+                </article>    
+                })}
         </Fragment>
     );
 };
