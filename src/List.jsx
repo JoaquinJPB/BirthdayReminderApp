cec26af2ed178a74a1bdad186d65d55abf35a05e
JoaquinJPB <jjpb2000@gmail.com>
2022-03-18 13:31:05 +0000

    birthday project setup

---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const List = () => {
+    return (
+        <Fragment>
+            <h2>list component</h2>
+        </Fragment>
+    );
+};
+
+export default List;
